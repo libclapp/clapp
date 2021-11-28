ff2eb4381aee5c0bd18b56fe8404f39220d0344c
Martin Wölzer <martin@libclapp.org>
2021-11-28 01:31:39 +0100

    cmake/clang_tidy.cmake: whitelist altera-unroll-loops for clang-tidy-13

--- cmake/clang_tidy.cmake
+++ cmake/clang_tidy.cmake
@@ -24,6 +24,10 @@ if(libClaPP_CLANG_TIDY_BIN)
         list(APPEND libClaPP_CLANG_TIDY_CHECKS_LIST -misc-no-recursion)
     endif()
 
+    if(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL "13.0.0")
+        list(APPEND libClaPP_CLANG_TIDY_CHECKS_LIST -altera-unroll-loops)
+    endif()
+
     set(libClaPP_CLANG_TIDY_CHECKS ${libClaPP_CLANG_TIDY_CHECKS_LIST})
     string(REPLACE ";" "," libClaPP_CLANG_TIDY_CHECKS "${libClaPP_CLANG_TIDY_CHECKS}")
 
