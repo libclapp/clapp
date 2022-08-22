201c86c417fe112ed354de8fad7f93e3e0b6c05a
Martin Wölzer <martin@libclapp.org>
2022-08-22 16:34:19 +0200

    tests/build_info.cpp: applied clang-tidy-14

--- tests/build_info.cpp
+++ tests/build_info.cpp
@@ -2,19 +2,21 @@
 #include <gmock/gmock.h>
 
 TEST(buildInfo, version) {
-    std::stringstream ss;
-    ss << clapp::build_info::version_major << "."
-       << clapp::build_info::version_minor << "."
-       << clapp::build_info::version_patch;
-    ASSERT_THAT(clapp::build_info::version, testing::StrEq(ss.str()));
+    std::stringstream string_str;
+    string_str << clapp::build_info::version_major << "."
+               << clapp::build_info::version_minor << "."
+               << clapp::build_info::version_patch;
+    ASSERT_THAT(clapp::build_info::version, testing::StrEq(string_str.str()));
 }
 
 TEST(buildInfo, buildInfoString) {
-    std::stringstream ss;
-    ss << clapp::build_info::project_name << " v." << clapp::build_info::version
-       << " " << clapp::build_info::build_type
-       << "-Build: " << clapp::build_info::git_branch << ":"
-       << clapp::build_info::git_hash << " ("
-       << clapp::build_info::git_description << ")";
-    ASSERT_THAT(clapp::build_info::build_info_string, testing::StrEq(ss.str()));
+    std::stringstream string_str;
+    string_str << clapp::build_info::project_name << " v."
+               << clapp::build_info::version << " "
+               << clapp::build_info::build_type
+               << "-Build: " << clapp::build_info::git_branch << ":"
+               << clapp::build_info::git_hash << " ("
+               << clapp::build_info::git_description << ")";
+    ASSERT_THAT(clapp::build_info::build_info_string,
+                testing::StrEq(string_str.str()));
 }
